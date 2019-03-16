dc37dae2d63607bc56cec72f2d1783fbea78c998
MK <martin.kalchev@mail.ru>
2019-03-16 15:24:51 +0200

    abbr add

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,4 +6,7 @@ abbr ar 'sudo apt remove'
 abbr au 'sudo apt-get update'
 abbr auu 'sudo apt-get upgrade'
 abbr gs 'git status'
-
+abbr gp 'git push'
+abbr gpl 'git pull'
+abbr gc 'git commit -m'
+abbr gf 'git fetch'
